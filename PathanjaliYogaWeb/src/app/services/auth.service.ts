f858c0bb1bdd7441cf99e27c8af57a871ea10966
thavahan <thavahan@gmail.com>
2026-03-12 16:41:00 +0530

    Hotfix login fallback endpoint for live route mismatch

--- PathanjaliYogaWeb/src/app/services/auth.service.ts
+++ PathanjaliYogaWeb/src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs';
+import { catchError, tap, throwError } from 'rxjs';
 import { Router } from '@angular/router';
 import { environment } from '../../environments/environment';
 
@@ -9,17 +9,20 @@ import { environment } from '../../environments/environment';
 })
 export class AuthService {
     private apiUrl = `${environment.apiUrl}/auth`;
+    private fallbackApiUrl = `${environment.apiUrl}/api/auth`;
 
     constructor(private http: HttpClient, private router: Router) { }
 
     login(credentials: any) {
         return this.http.post<any>(`${this.apiUrl}/login`, credentials).pipe(
+            catchError(() => this.http.post<any>(`${this.fallbackApiUrl}/login`, credentials)),
             tap(res => {
                 if (res.token) {
                     localStorage.setItem('yoga_token', res.token);
                     localStorage.setItem('yoga_user', res.username);
                 }
-            })
+            }),
+            catchError(err => throwError(() => err))
         );
     }
 
