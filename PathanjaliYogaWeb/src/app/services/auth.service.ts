cfa083a218c9c4f24ed78a9a5df2bf4a8534d905
thavahan <thavahan@gmail.com>
2026-03-12 16:23:49 +0530

    Fix live auth API path and remove hardcoded localhost URLs

--- PathanjaliYogaWeb/src/app/services/auth.service.ts
+++ PathanjaliYogaWeb/src/app/services/auth.service.ts
@@ -8,7 +8,7 @@ import { environment } from '../../environments/environment';
     providedIn: 'root'
 })
 export class AuthService {
-    private apiUrl = `${environment.apiUrl}/Auth`;
+    private apiUrl = `${environment.apiUrl}/auth`;
 
     constructor(private http: HttpClient, private router: Router) { }
 
